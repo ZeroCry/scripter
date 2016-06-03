d37ea958c5d80b19ffa91c037cac81bc80cec929
Joel Ibaceta <niohnex@gmail.com>
2016-06-03 13:06:25 -0300

    x-frame

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module ScriptSaver
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
+    config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'ALLOWALL'})
 
 
   end
